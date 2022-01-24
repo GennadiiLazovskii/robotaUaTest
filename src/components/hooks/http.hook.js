22d4c7ba88e2a7c1ece5c77f4c2a8d444fe0e533
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2022-01-24 20:47:48 +0200

    Fix

--- src/components/hooks/http.hook.js
+++ src/components/hooks/http.hook.js
@@ -1,6 +1,8 @@
+import { useCallback } from "react";
+
 export const useHttp = () => {
 
-    const request = async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
 
         try {
             const response = await fetch(url, {method, body, headers});
@@ -15,7 +17,7 @@ export const useHttp = () => {
         } catch(e) {
             throw e;
         }
-    };
+    }, []);
 
-    return {request}
+    return {reques}
 }
